8cc9f2b0b3026d2ad1e7562f870735a280c1015f
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-11-25 10:05:15 +0100

    allowedOrigin.js added

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'https://www.mysite.com',
+    'https://www.google.com',
+    'http://localhost:3000',
+    'http://localhost:3001',
+];
+
+module.exports = allowedOrigins;
