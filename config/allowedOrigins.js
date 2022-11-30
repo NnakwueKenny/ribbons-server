c0212bd4442c7457cc2defab1be3d2f4eab3c927
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-11-30 11:18:53 +0100

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
     'http://192.168.43.158',
+    'https://ribbons.vercel.app',
 ];
 
 module.exports = allowedOrigins;
