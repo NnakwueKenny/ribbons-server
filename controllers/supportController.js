264753f67dda0ea84eab9b631b12800dbce9aef1
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-12-06 10:08:32 +0100

    update

--- controllers/supportController.js
+++ controllers/supportController.js
@@ -4,58 +4,84 @@ const jwt = require('jsonwebtoken');
 
 
 // Send Complaint
-const sendSupport = async (req, res) => {
+const sendsupport = async (req, res) => {
     // console.log('Request Body:', req.body);
     // Generate random ID for the complaint
     const {v4: uuid} = require('uuid');
-    const {cat, name, desc, medium, status, loc, phone} = req.body;
-    console.log(cat, name, desc, medium, status, loc, phone);
-    const support = new Support({
-        cat: cat,
-        name: name,
-        desc: desc,
-        medium: medium,
-        status: status,
-        loc: loc,
-        phone: phone,
-        id: uuid()
-    })
+    const {cat, name, desc, medium, status, loc, phone, id} = req.body;
+    console.log(cat, name, desc, medium, status, loc, phone, id);
 
-    support.save()
-    .then(support => {
-        res.json(`Complaint saved successfully!`)
-    })
-    .catch(err => res.json({error: 'All fields are required!'}));
+    if (id) {
+        Support.findOne({id: id})
+        .then(support => {
+            console.log(support)
+            if (support) {
+                support.cat = cat;
+                support.name = name;
+                support.desc = desc;
+                support.medium = medium;
+                support.status = status;
+                support.loc = loc;
+                support.phone = phone;
+                support.id = id;
+                
+                support.save()
+                .then(support => {
+                    res.json(`Complaint saved successfully!`)
+                })
+                .catch(err => res.json({error: 'All fields are required!'}));
+            } else {
+                res.json({error: `No support with the following: ${id} found!`})
+            }
+        })
+    } else {
+        const support = new support({
+            cat: cat,
+            name: name,
+            desc: desc,
+            medium: medium,
+            status: status,
+            loc: loc,
+            phone: phone,
+            id: uuid()
+        })
+
+        support.save()
+        .then(support => {
+            res.json(`Complaint saved successfully!`)
+        })
+        .catch(err => res.json({error: 'All fields are required!'}));
+    }
 }
 
-const deleteSupport = async (req, res) => {
+const deletesupport = async (req, res) => {
     console.log('Hello');
     const { supportID } = req.body;
 
     Support.findOne({id: supportID})
     .then(complaint => {
         if (complaint) {
-            // Delete Support
+            // Delete support
             Support.findOneAndRemove({id: supportID})
             .then(() => {
                 res.json({
-                    message: `Complaint ${supportID} removed successfully!`
+                    message: `support ${supportID} deleted successfully!`
                 });
             })
             .catch(err => {
                 res.json({
-                    message: 'An error just occured!'
+                    message: 'An error just occured, while deleting the support!'
                 });
             });
         } else {
             return res.json({
-                message: 'Complaint not found'
+                message: 'support not found'
             });
         }
     })
 }
 
-const getAllSupport = (req, res) => {
+const getAllsupports = (req, res) => {
     const { loc, username} = req.body;
 
     Support.find({ loc: loc })
@@ -68,7 +94,7 @@ const getAllSupport = (req, res) => {
 }
 
 module.exports = {
-    sendSupport,
-    deleteSupport,
-    getAllSupport
+    sendsupport,
+    deletesupport,
+    getAllsupports
 }
